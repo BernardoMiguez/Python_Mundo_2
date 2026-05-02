597345d0f5f5390dc201c5968e127b7b3e5d78fb
BernardoMiguez <floogalsbernardo@gmail.com>
2026-05-01 23:56:15 -0300

    Create aula12.py

---
+++ Exercicios/aula12.py
@@ -0,0 +1,12 @@
+#estrutura condicional aninhada
+
+time = str(input("Qual é o time que você torce?: ")).capitalize()
+if time == "Flamengo":
+    print("Este é o melhor time do Brasil.")
+elif time == "Palmeiras":
+    print("Este é o pior time de São Paulo.")
+elif time == "Liverpool":
+    print("Este é o melhor time da Inglaterra.")
+elif time == "Atlético mineiro":
+    print("Este é o menor time de Minas.")
+print("Tenha um bom dia!")
